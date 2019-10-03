32bcc4280c705453a5cf0298843e4a8e3a9d1f0d
Drekrotion <Ramasik@gmail.com>
2019-10-03 21:42:34 +0300

    New Task for NodeJS

--- les_2/app.js
+++ les_2/app.js
@@ -5,13 +5,18 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.engine('.hbs', expHbs({
-    extname: '.hbs'
+    extname: '.hbs',
+    defaultLayout: null,
 }));
 
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'static'));
+
+const users = [];
 
 app.get('/', (req, res) => {
     res.render('main');
@@ -22,22 +27,37 @@ app.get('/', (req, res) => {
 // console.log(__filename);
 
 app.get('/login', (req, res) => {
-    res.end('LOGIN PAGE')
+    res.render('login')
 });
 
 app.get('/register', (req, res) => {
-    res.end('REGISTER PAGE')
+    res.render('register')
+
+});
+app.post('/register', (req, res) => {
+    const body = req.body;
+    body['body_id'] = users.length + 1;
+    users.push(body);
+    console.log(body);
+
 });
 
 
+app.post('/login', (req, res) => {
+    const body = req.body;
+    users.forEach(user => {
+        if (user.userName === body.userName & user.password === body.password){
+            res.redirect()
+        }
+    })
 
+});
 
-
-app.all('*', (req, res)=> {
+app.all('*', (req, res) => {
     res.json('404 NOT FOUND')
 });
 
 app.listen(3000, () => {
-    console.log('3000-HELLO');
+    console.log('GO GO GO GO GO');
 });
 
